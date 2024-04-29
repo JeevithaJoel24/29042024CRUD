ce999865969131044379aca0d7550296fe39244d
Jeevitha Jones <jeevithajoel24@gmail.com>
2024-04-29 15:45:16 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import { Button, Table } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import array from "./array";    
+
+function Home(){
+    return <div>
+       <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                {array.map(item => 
+                    <tr>
+                        <td>{item.Name}</td>
+                        <td>{item.Age}</td>
+                    </tr>
+                )}
+            </tbody>
+            </Table>
+    </div>
+}
+
+
+export default Home
